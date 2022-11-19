d7cfc4cb62bb2c8d95f70a0c87686f513f21f41a
Francis Stokes <stokesie14@gmail.com>
2022-11-19 13:17:58 +0100

    First working version

---
+++ src/example/tiles.ts
@@ -0,0 +1,20 @@
+import * as path from 'path';
+import * as fsSync from 'fs';
+import { block, inline, inlineBytes } from '../utils';
+
+const tileData = fsSync.readFileSync(path.join(__dirname, 'tiledata.bin'));
+const titleMapData = fsSync.readFileSync(path.join(__dirname, 'titlemap.bin'));
+const tileMapData = fsSync.readFileSync(path.join(__dirname, 'map.bin'));
+const gameOverMapData = fsSync.readFileSync(path.join(__dirname, 'gameovermap.bin'));
+
+export const tiles = block('Tiles', () => [inlineBytes(tileData)]);
+export const titleMap = block('TitleMap', () => [inlineBytes(titleMapData)]);
+export const tileMap = block('TileMap', () => [inlineBytes(tileMapData)]);
+export const gameOverMap = block('gameOverMap', () => [inlineBytes(gameOverMapData)]);
+
+export const allTileData = inline([
+  tiles.block,
+  tileMap.block,
+  titleMap.block,
+  gameOverMap.block,
+]);
